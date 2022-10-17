08f1ecf09c67cc1138b3199aa8549a2452b14ec3
jmmirza <janmuhammadmirza@gmail.com>
2022-10-17 22:38:45 +0500

    changes

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -91,7 +91,12 @@
                                 Payment Method
                             </a>
                         </li>
-
+                        <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="tab" href="#business_info" role="tab">
+                                <i class="far fa-envelope"></i>
+                                Business Information
+                            </a>
+                        </li>
                     </ul>
                 </div>
                 <div class="card-body p-4">
@@ -363,6 +368,221 @@
                                                 @endforelse
                                             </tbody>
                                         </table>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane" id="business_info" role="tabpanel">
+                            <div class="card-header align-items-center d-flex">
+                                <h4 class="card-title mb-0 flex-grow-1">Bank Details</h4>
+                                {{-- @permission('add-course') --}}
+                                <div class="flex-shrink-0">
+                                    <a href="{{ route('itinerary-templates.create') }}"
+                                        class="btn btn-success btn-label btn-sm">
+                                        <i class="ri-add-fill label-icon align-middle fs-16 me-2"></i> Add New
+                                    </a>
+                                </div>
+                                {{-- @endpermission --}}
+                            </div>
+                            <div class="card">
+                                <div class="card-body">
+                                    <form action="javascript:void(0);">
+                                        <div class="row">
+                                            <div class="col-lg-4">
+                                                <div class="form-label-group in-border mb-3">
+                                                    <label for="companyTitle" class="form-label">Company Title</label>
+                                                    <input type="text"
+                                                        class="form-control @if ($errors->has('companyTitle')) is-invalid @endif"
+                                                        id="companyTitle" name="companyTitle" placeholder="Please Enter"
+                                                        value="{{ Auth::user()->companyTitle }}">
+                                                    <div class="invalid-tooltip">
+                                                        @if ($errors->has('companyTitle'))
+                                                            {{ $errors->first('companyTitle') }}
+                                                        @else
+                                                            Company Title is required!
+                                                        @endif
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-4">
+                                                {{-- <label for="cityInput" class="form-label">City</label> --}}
+                                                <div class="form-label-group in-border mb-3">
+                                                    <label for="cityId" class="form-label">City</label>
+                                                    <select
+                                                        class="form-select form-control mb-3 @if ($errors->has('cityId')) is-invalid @endif"
+                                                        name="cityId">
+                                                        <option value=""
+                                                            @if (Auth::user()->cityId == '') {{ 'selected' }} @endif
+                                                            selected disabled>
+                                                            Select One
+                                                        </option>
+                                                        @foreach ($cities as $city)
+                                                            <option value="{{ $city->city_id }}"
+                                                                @if (Auth::user()->cityId == $city->city_id) {{ 'selected' }} @endif>
+                                                                {{ $city->title }}
+                                                            </option>
+                                                        @endforeach
+                                                    </select>
+                                                    <div class="invalid-tooltip">
+                                                        @if ($errors->has('cityId'))
+                                                            {{ $errors->first('cityId') }}
+                                                        @else
+                                                            Select the City!
+                                                        @endif
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-4">
+                                                <div class="form-label-group in-border mb-3">
+                                                    <label for="companyAddress" class="form-label">Address</label>
+                                                    <input type="text"
+                                                        class="form-control @if ($errors->has('companyAddress')) is-invalid @endif"
+                                                        id="companyAddress" name="companyAddress"
+                                                        placeholder="Please Enter"
+                                                        value="{{ Auth::user()->companyAddress }}">
+                                                    <div class="invalid-tooltip">
+                                                        @if ($errors->has('companyAddress'))
+                                                            {{ $errors->first('companyAddress') }}
+                                                        @else
+                                                            Address is required!
+                                                        @endif
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-4">
+                                                <div class="form-label-group in-border mb-3">
+                                                    <label for="contactNumber" class="form-label">Phone Number</label>
+                                                    <input type="text"
+                                                        class="form-control @if ($errors->has('contactNumber')) is-invalid @endif"
+                                                        id="contactNumber" name="contactNumber"
+                                                        placeholder="Please Enter"
+                                                        value="{{ Auth::user()->contactNumber }}">
+                                                    <div class="invalid-tooltip">
+                                                        @if ($errors->has('contactNumber'))
+                                                            {{ $errors->first('contactNumber') }}
+                                                        @else
+                                                            Phone Number is required!
+                                                        @endif
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-4">
+                                                <div class="form-label-group in-border mb-3">
+                                                    <label for="businessEmail" class="form-label">Email</label>
+                                                    <input type="text"
+                                                        class="form-control @if ($errors->has('businessEmail')) is-invalid @endif"
+                                                        id="businessEmail" name="businessEmail"
+                                                        placeholder="Please Enter"
+                                                        value="{{ Auth::user()->businessEmail }}">
+                                                    <div class="invalid-tooltip">
+                                                        @if ($errors->has('businessEmail'))
+                                                            {{ $errors->first('businessEmail') }}
+                                                        @else
+                                                            Email is required!
+                                                        @endif
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-4">
+                                                <div class="form-label-group in-border mb-3">
+                                                    <label for="contactPerson" class="form-label">Contact Person</label>
+                                                    <input type="text"
+                                                        class="form-control @if ($errors->has('contactPerson')) is-invalid @endif"
+                                                        id="contactPerson" name="contactPerson"
+                                                        placeholder="Please Enter"
+                                                        value="{{ Auth::user()->contactPerson }}">
+                                                    <div class="invalid-tooltip">
+                                                        @if ($errors->has('contactPerson'))
+                                                            {{ $errors->first('contactPerson') }}
+                                                        @else
+                                                            Contact Person is required!
+                                                        @endif
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-6">
+                                                {{-- <label for="cityInput" class="form-label">City</label> --}}
+                                                <div class="form-label-group in-border mb-3">
+                                                    <label for="businessType" class="form-label">Business Type</label>
+                                                    <select
+                                                        class="form-select form-control mb-3 @if ($errors->has('businessType')) is-invalid @endif"
+                                                        name="businessType">
+                                                        <option
+                                                            @if (Auth::user()->businessType == '') {{ 'selected' }} @endif
+                                                            value="">Select</option>
+                                                        <option
+                                                            @if (Auth::user()->businessType == '0') {{ 'selected' }} @endif
+                                                            value="0">FIT and Small GROUP Business</option>
+                                                        <option
+                                                            @if (Auth::user()->businessType == '1') {{ 'selected' }} @endif
+                                                            value="1">Mainly MICE Business</option>
+                                                        <option
+                                                            @if (Auth::user()->businessType == '2') {{ 'selected' }} @endif
+                                                            value="2">Mainly Group Business</option>
+                                                    </select>
+                                                    <div class="invalid-tooltip">
+                                                        @if ($errors->has('businessType'))
+                                                            {{ $errors->first('businessType') }}
+                                                        @else
+                                                            Select the Business Type!
+                                                        @endif
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-6">
+                                                {{-- <label for="cityInput" class="form-label">City</label> --}}
+                                                <div class="form-label-group in-border mb-3">
+                                                    <label for="typeDescription" class="form-label">Description</label>
+                                                    <select
+                                                        class="form-select form-control mb-3 @if ($errors->has('typeDescription')) is-invalid @endif"
+                                                        name="typeDescription">
+                                                        <option
+                                                            @if (Auth::user()->typeDescription == '') {{ 'selected' }} @endif
+                                                            value="">Select</option>
+                                                        <option
+                                                            @if (Auth::user()->typeDescription == '1') {{ 'selected' }} @endif
+                                                            value="1">Tour Operator mainly focusing on Inbound
+                                                        </option>
+                                                        <option
+                                                            @if (Auth::user()->typeDescription == '2') {{ 'selected' }} @endif
+                                                            value="2">Travel Agent mainly focusing on Outbound
+                                                        </option>
+                                                    </select>
+                                                    <div class="invalid-tooltip">
+                                                        @if ($errors->has('businessType'))
+                                                            {{ $errors->first('businessType') }}
+                                                        @else
+                                                            Select the Description!
+                                                        @endif
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-12 col-sm-12 mb-3">
+                                                <label for="about" class="form-label">About</label>
+                                                <div id="snow-editor" style="height: 300px;">{!! Auth::user()->about !!}</div>
+                                                <input type="hidden" name="about" id="about"
+                                                    value="{{ Auth::user()->about }}">
+                                                {{-- <div class="form-label-group in-border">
+                                    <label for="description" class="form-label">Description (物品描述)</label>
+                                    <textarea class="form-control mb-3" name="description" id="description" placeholder="Enter product description here...">{{ $paymentMethod->description') }}</textarea>
+                                </div> --}}
+                                            </div>
+
+                                            <div class="col-lg-12">
+                                                <div class="hstack gap-2 justify-content-end">
+                                                    <button type="submit" class="btn btn-primary">Updates</button>
+                                                    <button type="button" class="btn btn-soft-success">Cancel</button>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </form>
                                 </div>
                             </div>
