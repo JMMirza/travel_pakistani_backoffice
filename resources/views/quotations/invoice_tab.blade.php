703897ebc67a44242d336aa8f373d0e8e6dc5912
9meAhmed <noman.ahmed1408@gmail.com>
2022-11-04 22:37:22 +0500

    changes

--- resources/views/quotations/invoice_tab.blade.php
+++ resources/views/quotations/invoice_tab.blade.php
@@ -1,10 +1,10 @@
 <div class="tab-pane {{ $tab == 7 ? 'active' : '' }}" id="nav-border-top-06" role="tabpanel">
-    <form class="needs-validation" id="quotationForm" novalidate action="{{ route('save-quotation') }}" method="POST">
+
+    <form class="needs-validation" novalidate method="POST" action="{{ route('save-quotation-invoice') }}">
+
         @csrf
-        <input type="hidden" value="{{ isset($quotation->id) ? $quotation->id : 0 }}" name="quotationId" />
-        <input type="hidden" value="{{ isset($totalPersons) ? $totalPersons : 0 }}" name="personsCount" id="personsCount" />
-
-
+        <input type="hidden" value="{{ isset($quotation) ? $quotation->id : 0 }}" name="quotationId" />
+        <input type="hidden" value="{{ isset($quotation) ? $quotation->versionNo : 1 }}" name="quotationVersion" />
         <div class="table-responsive ">
             <table class="table align-middle table-nowrap  mb-3">
                 <thead class="table-light">
@@ -17,39 +17,32 @@
             </table>
         </div>
 
-        <form method="POST" action="{{ route('save-quotation-invoice') }}">
-            @csrf
-            <input type="hidden" value="{{ isset($quotation) ? $quotation->id : 0 }}" name="quotationId" />
-            <input type="hidden" value="{{ isset($quotation) ? $quotation->versionNo : 1 }}" name="quotationVersion" />
-            <div class="row g-3">
-                <div class="col-md-12">
-                    <label for="staffRemarks" class="form-label">Description </label>
-                    <textarea class="form-control" id="description" placeholder="Enter description" name="description"></textarea>
-                </div>
-                <div class="col-md-3">
-                    <label for="dueAmount" class="form-label">Amount <span class="text-danger">*</span></label>
-                    <input type="number" value="" class="form-control" id="dueAmount" placeholder="Enter due amount" name="dueAmount" required>
-                </div>
-                <div class="col-md-3">
-                    <label for="remainingAmount" class="form-label">Remaining Amount </label>
-                    <input type="number" value="" class="form-control" id="remainingAmount" placeholder="Enter remaining amount" name="remainingAmount" readonly required>
-                </div>
-                <div class="col-md-3">
-                    <label for="invoiceDate" class="form-label">Invoice Date <span class="text-danger">*</span></label>
-                    <input value="" type="text" class="form-control" id="invoiceDate" placeholder="Enter due date" name="invoiceDate" required>
-                </div>
-                <div class="col-md-3">
-                    <label for="dueDate" class="form-label">Due Date <span class="text-danger">*</span></label>
-                    <input value="" type="text" class="form-control" id="dueDate" placeholder="Enter due date" name="dueDate" required>
-                </div>
-                <div class="col-12">
-                    <button type="button" class="btn btn-light">Back</button>
-                    <button type="submit" class="btn btn-primary">Save Changes</button>
-                </div>
+        <div class="row g-3">
+            <div class="col-md-12">
+                <label for="staffRemarks" class="form-label">Description </label>
+                <textarea class="form-control" id="description" placeholder="Enter description" name="description"></textarea>
             </div>
-        </form>
-
-
+            <div class="col-md-3">
+                <label for="dueAmount" class="form-label">Amount <span class="text-danger">*</span></label>
+                <input type="number" value="" class="form-control" id="dueAmount" placeholder="Enter due amount" name="dueAmount" required>
+            </div>
+            <div class="col-md-3">
+                <label for="remainingAmount" class="form-label">Remaining Amount </label>
+                <input type="number" value="" class="form-control" id="remainingAmount" placeholder="Enter remaining amount" name="remainingAmount" readonly required>
+            </div>
+            <div class="col-md-3">
+                <label for="invoiceDate" class="form-label">Invoice Date <span class="text-danger">*</span></label>
+                <input value="" type="text" class="form-control" id="invoiceDate" placeholder="Enter due date" name="invoiceDate" required>
+            </div>
+            <div class="col-md-3">
+                <label for="dueDate" class="form-label">Due Date <span class="text-danger">*</span></label>
+                <input value="" type="text" class="form-control" id="dueDate" placeholder="Enter due date" name="dueDate" required>
+            </div>
+            <div class="col-12">
+                <button type="button" class="btn btn-light">Back</button>
+                <button type="submit" class="btn btn-primary">Save Changes</button>
+            </div>
+        </div>
     </form>
 </div>
 
@@ -59,6 +52,7 @@
 
         $('#dueDate').flatpickr({
             minDate: "today",
+            defaultDate: "today",
             // mode: "range",
             altInput: true,
             altFormat: "F j, Y",
@@ -68,6 +62,8 @@
         $('#invoiceDate').flatpickr({
             // minDate: "today",
             // mode: "range",
+            defaultDate: "today",
+
             altInput: true,
             altFormat: "F j, Y",
             dateFormat: "Y-m-d",
